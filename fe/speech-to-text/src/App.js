11ac83a4ff5ada275b1f73cb4712c70a47d82ba1
Athaya Reyhan <athreyhan04@gmail.com>
2023-05-18 14:57:31 +0700

    bisa get city sekarang

--- fe/speech-to-text/src/App.js
+++ fe/speech-to-text/src/App.js
@@ -1,5 +1,6 @@
 import Homepage from "./pages/Homepage";
 import Hospitals from "./pages/Hospitals";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <div className="App">
         <Router>
+          <Navbar/>
           <Routes>
             <Route path={"/"} element={<Homepage />} />
             <Route path={"/hospitals"} element={<Hospitals />} />
--- fe/speech-to-text/src/pages/Hospitals.jsx
+++ fe/speech-to-text/src/pages/Hospitals.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 
 const Hospitals = () => {
+  const [currentCity, setCurrentCity] = useState("")
+
+  const getCurrentCity = async (position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    const res = await axios
+      .get(
+        `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=jsonv2`
+      )
+      .then((response) => {
+        const city = response.data.address.city
+        setCurrentCity(city)
+      });
+    console.log(currentCity);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCurrentCity);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }, [currentCity]);
+
+
   return (
     <>
       <div className="container">
-        <p>Nearest Hospitals</p>
+        <h2>Nearest Hospitals</h2>
+        <p>You are at {currentCity}</p>
         <ul>
           <li></li>
         </ul>
