8634f5ac978ba4c16d440333228f38adcf41ad57
treeoon <wisnuyeahfaizal@gmail.com>
2023-05-19 14:40:36 +0700

    fix modal

--- fe/speech-to-text/src/pages/Homepage.jsx
+++ fe/speech-to-text/src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MedicineList from "../components/MedicineList";
 import axios from "axios";
 
 const SpeechRecognition =
@@ -25,22 +26,6 @@ const Homepage = () => {
       .then((response) => {
         const api = response.data.result;
         setMedicine(api);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  async function getMedicineDetail(slug) {
-    await axios
-      .get(`http://localhost:5000/api/buy-medicine/products/details`, {
-        params: {
-          query: slug,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        alert(response.data.description);
       })
       .catch((error) => {
         console.error(error);
@@ -146,31 +131,7 @@ const Homepage = () => {
             <button onClick={handleClearDiagnoze}> Clear Diagnoze </button>
           </div>
         </div>
-        <div className="w-full h-full mt-3 grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-7 p-2 px-7 ">
-          {medicine
-            ? medicine.map((med) => (
-                <div
-                  className=" p-4 shadow-md border-gray-200 border-[1px]"
-                  key={med.external_id}
-                >
-                  <p>{med.name}</p>
-                  <img
-                    src={med.image_url}
-                    alt={med.name}
-                    className="w-40 mx-auto"
-                  />
-                  <p>
-                    Range Harga: Rp.{med.min_price} - Rp.{med.base_price}
-                  </p>
-                  <button
-                    onClick={async () => await getMedicineDetail(med.slug)}
-                  >
-                    Details
-                  </button>
-                </div>
-              ))
-            : ""}
-        </div>
+        <MedicineList medicine={medicine} />
       </div>
     </>
   );
