5ef8c82cf19973f9ad90b6b86da1087146fd6c57
treeoon <wisnuyeahfaizal@gmail.com>
2023-05-22 17:15:19 +0700

    fix: hospital

--- fe/speech-to-text/src/pages/Hospitals.jsx
+++ fe/speech-to-text/src/pages/Hospitals.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-
 const Hospitals = () => {
   const [currentCity, setCurrentCity] = useState("");
   const [hospitals, setHospitals] = useState([]);
@@ -16,50 +15,63 @@ const Hospitals = () => {
 
   useEffect(() => {
     if (currentCity) {
-      console.log(currentCity)
-      getNearestHospitals(currentCity);
+      searchNearestHospitals();
     }
   }, [currentCity]);
+
+  const getDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1); // deg2rad below
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return d;
+  };
+
+  const deg2rad = (deg) => {
+    return deg * (Math.PI / 180);
+  };
 
   const getCurrentCity = async (position) => {
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
 
-    const res = await axios
+    await axios
       .get(
         `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=jsonv2`
       )
       .then((response) => {
         const city = response.data.address.city;
-        setCurrentCity(city);
-      });
 
-      const response = axios.get(
-        `https://nominatim.openstreetmap.org/search.php?q=hospital+in+${currentCity}&format=json&bounded=1&viewbox=${
-          long - 0.5
-        },${lat - 0.5},${long + 0.5},${lat + 0.5}&limit=10`, () => {
-          try {
-            const address = response.data.address;
-            console.log(address);
-          } catch (error) {
-            throw error;
-          }
-        }
-      );
+        searchNearestHospitals(lat, long);
+        setCurrentCity(city);
+
+        return long;
+      });
   };
 
-  const getNearestHospitals = (currentCity) => {
+  const searchNearestHospitals = (lat, long) => {
+    getCurrentCity();
+
     axios
-      .put(`http://localhost:5000/hospital/location`, {
-        search: currentCity,
-      })
+      .get(
+        `https://nominatim.openstreetmap.org/search.php?q=hospital+in+${currentCity}&format=json&bounded=1&viewbox=${
+          long - 0.5
+        },${lat - 0.5},${long + 0.5},${lat + 0.5}&limit=10`
+      )
       .then((response) => {
-        let hospitals = response.data;
-        setHospitals(hospitals);
+        let hospital = response.data;
+        hospital.forEach((h) => {
+          h.distance = getDistance(lat, long, h.lat, h.lon);
+        });
+        setHospitals(hospital);
         console.log(hospitals);
-      })
-      .catch((error) => {
-        throw error;
       });
   };
 
@@ -69,13 +81,16 @@ const Hospitals = () => {
         <h2>Nearest Hospitals</h2>
         <p>You are in {currentCity}</p>
         <div>
-          {/* {hospitals
-            ? hospitals.map((h) => (
-                <>
-                  <p>{h.name}</p>
-                </>
-              ))
-            : null} */}
+          {hospitals
+            ? hospitals
+                .sort((a, b) => a.distance - b.distance)
+                .map((h) => (
+                  <>
+                    <p>{h.display_name}</p>
+                    <p>Jarak : {h.distance.toFixed(2)} km</p>
+                  </>
+                ))
+            : null}
         </div>
       </div>
     </>
