4fd5a7be986b13099830136f571e5c61b641771c
dzakiruw <dzakiruw@gmail.com>
2023-05-29 03:22:46 +0700

    feat: fix bookmark

--- fe/speech-to-text/src/components/Medicine/MedicineListAlo.jsx
+++ fe/speech-to-text/src/components/Medicine/MedicineListAlo.jsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function MedicineList({ medicine }) {
-  // fungsi untuk bookmark
+export default function MedicineListAlo({ medicine }) {
+  const [bookmarkedMedicines, setBookmarkedMedicines] = useState(JSON.parse(localStorage.getItem("bookmarkedMedicines") || "[]"));
+
+  useEffect(() => {
+    setBookmarkedMedicines(JSON.parse(localStorage.getItem("bookmarkedMedicines") || "[]"));
+  }, []);
+
+  const isBookmarked = (med) => {
+    return bookmarkedMedicines.some((medicine) => medicine.id === med.id);
+  };
+
   const bookmarkMedicine = (med) => {
-    let bookmarkedMedicines = JSON.parse(localStorage.getItem("bookmarkedMedicines") || "[]");
-    let exists = bookmarkedMedicines.some(x => x.id === med.id);
-    if (!exists) {
-      bookmarkedMedicines.push(med);
-      localStorage.setItem("bookmarkedMedicines", JSON.stringify(bookmarkedMedicines));
+    if (!isBookmarked(med)) {
+      const newBookmarkedMedicines = [...bookmarkedMedicines, med];
+      setBookmarkedMedicines(newBookmarkedMedicines);
+      localStorage.setItem("bookmarkedMedicines", JSON.stringify(newBookmarkedMedicines));
     }
   };
 
-  // fungsi untuk unbookmark
   const unbookmarkMedicine = (med) => {
-    let bookmarkedMedicines = JSON.parse(localStorage.getItem("bookmarkedMedicines") || "[]");
-    let newBookmarkedMedicines = bookmarkedMedicines.filter(x => x.id !== med.id);
+    const newBookmarkedMedicines = bookmarkedMedicines.filter((medicine) => medicine.id !== med.id);
+    setBookmarkedMedicines(newBookmarkedMedicines);
     localStorage.setItem("bookmarkedMedicines", JSON.stringify(newBookmarkedMedicines));
   };
 
-  // fungsi untuk cek apakah sudah dibookmark
-  const isBookmarked = (med) => {
-    let bookmarkedMedicines = JSON.parse(localStorage.getItem("bookmarkedMedicines") || "[]");
-    return bookmarkedMedicines.some(x => x.id === med.id);
-  };
   return (
     <div className="mt-3 grid h-full w-full grid-cols-1 gap-7 p-2 px-8 sm:mt-5 sm:grid-cols-2 sm:p-0 md:mt-5 md:gap-11 lg:grid-cols-3">
       {medicine
@@ -43,10 +45,6 @@ export default function MedicineList({ medicine }) {
               <p className="mx-auto font-inter text-base font-semibold text-dblack">
                 IDR {med.price.display_amount}
               </p>
-              {isBookmarked(med) ? 
-            <button onClick={() => unbookmarkMedicine(med)}>Unbookmark</button> : 
-            <button onClick={() => bookmarkMedicine(med)}>Bookmark</button>
-          }
               <div className="flex w-full justify-between">
                 <span className="mx-auto font-inter text-base font-semibold text-dblack">
                   Rating: {med.rating}
@@ -65,6 +63,12 @@ export default function MedicineList({ medicine }) {
                   Lihat Detail
                 </button>
               </Link>
+              <button
+                  onClick={() => isBookmarked(med) ? unbookmarkMedicine(med) : bookmarkMedicine(med)}
+                  className={`w-full rounded-lg py-2 font-nunito text-base font-extrabold ${isBookmarked(med) ? "bg-red-500 text-white" : "bg-blue-500 text-white"} sm:text-lg`}
+                >
+                  {isBookmarked(med) ? "Bookmarked" : "Bookmark"}
+                </button>
             </div>
           ))
         : ""}
